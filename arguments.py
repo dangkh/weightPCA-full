934af2c15330b0011678e74b6e6d977ba1d46afb
dangkh <dangkh109@gmail.com>
2022-03-28 05:21:50 +0700

    updateR1

--- arguments.py
+++ arguments.py
@@ -8,40 +8,40 @@ class arguments(argparse.Namespace):
     length3D = 400
     
 
-    #  # """ CMU config """
-    # missing_index = [750, 1150]
-    # # # current result
-    # inner_testing = [ [1250, 1650], [550, 950], [950, 1350], [2250, 2650]]
+     # """ CMU config """
+    missing_index = [1750, 2150]
+    # # current result
+    inner_testing = [ [1250, 1650], [550, 950], [950, 1350], [2250, 2650]]
     
-    # collection_link = ["./data3D/135_01.txt", "./data3D/135_03.txt"]
-    # outter_testing = [[1450, 1850], [1450, 1850]]
-    # data_link = "./data3D/CMU2.txt"
-    # # # single case
-    # # # weightScale = 2000
-    # # # MMweight = 0.001
-
-    # # # mul case
-    # # # weightScale = 2000
-    # # # MMweight = 0.02
-    # # # others = 0.001
-    
-
-    
-    """ HDM config """
-    missing_index = [1650, 2050]
-    
-    inner_testing = [[1050, 1450], [1450, 1850], [200, 600], [2000, 2400]]
-    collection_link = ["./data3D/HDM3.txt", "./data3D/HDM4.txt"]
+    collection_link = ["./data3D/135_01.txt", "./data3D/135_03.txt"]
     outter_testing = [[1450, 1850], [1450, 1850]]
-    data_link = "./data3D/HDM5.txt"
-    # single case
-    # weightScale = 500
-    # MMweight = 0.001
+    data_link = "./data3D/135_02.txt"
+    # # single case
+    # # weightScale = 2000
+    # # MMweight = 0.001
 
-    # mul case
-    # weightScale = 500
-    # MMweight = 0.02
-    # others = 0.001
+    # # mul case
+    # # weightScale = 2000
+    # # MMweight = 0.02
+    # # others = 0.001
+    
+
+    
+    # """ HDM config """
+    # missing_index = [1650, 2050]
+    
+    # inner_testing = [[1050, 1450], [1450, 1850], [200, 600], [2000, 2400]]
+    # collection_link = ["./data3D/HDM3.txt", "./data3D/HDM4.txt"]
+    # outter_testing = [[1450, 1850], [1450, 1850]]
+    # data_link = "./data3D/HDM5.txt"
+    # # single case
+    # # weightScale = 500
+    # # MMweight = 0.001
+
+    # # mul case
+    # # weightScale = 500
+    # # MMweight = 0.02
+    # # others = 0.001
 
 
     tmp_AN3 = []
